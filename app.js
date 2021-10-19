d1d3868398ff35209e0259d286cc3e32f6ab2dcc
Solntsev Vitaly <34040342+SolntsevV@users.noreply.github.com>
2021-10-19 22:54:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 export default function appSrc(express, bodyParser, createReadStream, crypto, http) {
   const app = express()
   app.use(bodyParser.text())
+  
+  app.get('/', (req, res) => {
+    res.send('Hello World!'); 
+  });
+  
   return app;
 }
