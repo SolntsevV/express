becb4f02808ed117e4039edb3d076235c441cab7
Solntsev Vitaly <34040342+SolntsevV@users.noreply.github.com>
2021-10-19 22:00:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-import { Server } from 'http';
+const express = require('express')
+
+const app = express()
+
+
+app.all('/', (req, res) => {
+  
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS'
+  });
+  res.send('vitaly_solntsev');
+
+});
+
+app.listen(process.env.PORT);
