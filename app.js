9f5d5b3f7a7ba6375592ff9029212c13f93da9c6
Solntsev Vitaly <34040342+SolntsevV@users.noreply.github.com>
2021-10-19 22:57:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ export default function appSrc(express, bodyParser, createReadStream, crypto, ht
   const app = express()
   app.use(bodyParser.text())
   
-  app.get('/', (req, res) => {
-    res.send('Hello World!'); 
-  });
+  app.all('/login/', (req, res) => {
+    res.set({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS'
+    });
+    res.send('vitaly_solntsev');
+})
   
   return app;
 }
