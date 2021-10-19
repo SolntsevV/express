2c2caf479e374a148b7140e04d334b1ef7e3afab
Solntsev Vitaly <34040342+SolntsevV@users.noreply.github.com>
2021-10-19 22:23:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const PORT = 4321
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen()
+app.listen(process.env.PORT || PORT, () => log(process.pid))
