0ab4834f226e94812c793fa8f563e676c2057b94
Solntsev Vitaly <34040342+SolntsevV@users.noreply.github.com>
2021-10-19 22:52:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,4 @@ import http from 'http';
 
 const app = appSrc(express, bodyParser, createReadStream, crypto, http);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(process.env.PORT || 4321)
