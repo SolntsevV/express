2c7fcd64a70b8b141380700d6156b3fb57f4a037
Solntsev Vitaly <34040342+SolntsevV@users.noreply.github.com>
2021-10-19 23:00:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ export default function appSrc(express, bodyParser, createReadStream, crypto, ht
       'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS'
     });
     res.send('vitaly_solntsev');
-})
+  })
+  
+  app.all('/code/', (req, res) => {
+    res.set({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS'
+    });
+    res.send('code');
+  })
   
   return app;
 }
