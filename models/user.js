096900525685d70e9704661714f879ce6d7b8704
Solntsev Vitaly <34040342+SolntsevV@users.noreply.github.com>
2021-10-21 18:54:49 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 export default m => {
     const UserSchema = m.Schema({
         login: String,
-        password: String
+        password: {
+            type: String,
+            required: [true, 'Password is a necessity!']
+        }
     })
     return m.model('User', UserSchema);
 }
